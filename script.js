7f65eef1a1102ac42c537ca4da5434bb02f1d290
Aditya <holaaditya123@gmail.com>
2024-03-09 04:28:08 +0530

    Update index.html and login.css

---
+++ script.js
@@ -0,0 +1,11 @@
+function showDialog() {
+  var dialogBox = document.getElementById("dialog-box");
+  dialogBox.classList.remove("dialog-hidden");
+  dialogBox.classList.add("dialog-visible");
+}
+
+function hideDialog() {
+  var dialogBox = document.getElementById("dialog-box");
+  dialogBox.classList.remove("dialog-visible");
+  dialogBox.classList.add("dialog-hidden");
+}
